bf79301ed84637bdd0a8a26fdb8ddb25b3fb4df2
Piotr Gorczyński <p.gorczynski4@gmail.com>
2025-12-09 00:12:44 +0100

    Initial commit

---
+++ src/main/java/com/example/simplifyStorePrime/SimplifyStorePrimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplifyStorePrime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifyStorePrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifyStorePrimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplifyStorePrime/SimplifyStorePrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplifyStorePrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifyStorePrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
